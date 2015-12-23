5d7cd7b7a33ba6f1ddeffa067745568cbbfd7c2c
Shawn Lim <shawnlim.ws@gmail.com>
2015-12-24 00:55:42 +0800

    made responsive

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-const box = document.querySelector('.box');
-const boxContents = document.querySelector('.boxContents');
-const previousWork = document.querySelector('.previousWork');
-const workLink = document.querySelector('.workLink');
-const close = document.querySelector('.fa-times-circle-o');
+'use strict';
+
+var box = document.querySelector('.box');
+var boxContents = document.querySelector('.boxContents');
+var previousWork = document.querySelector('.previousWork');
+var workLink = document.querySelector('.workLink');
+var close = document.querySelector('.fa-times-circle-o');
 
 workLink.onclick = function () {
   box.classList.add('expand');
